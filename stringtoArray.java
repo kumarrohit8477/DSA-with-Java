589d36051b3a6965eef5153d601ea12beaba4d80
kumarrohit8477 <kumarrohitt0976@gmail.com>
2025-08-21 17:09:06 +0530

    add new file

---
+++ stringtoArray.java
@@ -0,0 +1,9 @@
+public class stringtoArray {
+   public static void main(String[] args) {
+      String str = "Rohit Kumar";
+      char[] arr = str.toCharArray();
+      System.out.println(arr);
+      char[] arr2 = {'R', 'o', 'h', 'i', 't'};
+      System.out.println(arr2);
+   }
+}
